70723f766ba18b30b175dd6f648358f134e6e71d
Nastya Savinova <savinovaag@gmail.com>
2023-12-03 19:49:44 +0300

    Создан массив

---
+++ Z3/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 3: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с
+// наименьшей суммой элементов.
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GenerateMatrix(int row, int col, int leftRange, int rightRange)
+{
+    int[,] tempMatrix = new int[row, col];
+    Random rand = new Random();
+
+    for (int i = 0; i < tempMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < tempMatrix.GetLength(1); j++)
+        {
+            tempMatrix[i, j] = rand.Next(leftRange, rightRange + 1);
+        }
+    }
+
+    return tempMatrix;
+}
+
+void PrintMatrix(int[,] matrixForPrint)
+{
+    for (int i = 0; i < matrixForPrint.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrixForPrint.GetLength(1); j++)
+        {
+            System.Console.Write(matrixForPrint[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+///--------
+
+
+
+///----------
+int rows = ReadInt("Введите количество строк в матрице: ");
+
+int cols = ReadInt("Введите количество столбцов в матрице: ");
+
+int[,] matrix = GenerateMatrix(rows, cols, 1, 10);
+
+PrintMatrix(matrix);
+
