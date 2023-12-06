64950dabb573618fa66e4b0a8bb351f8cf4ca600
Nastya Savinova <savinovaag@gmail.com>
2023-12-06 20:51:01 +0300

    Задача 4  решена

--- Z4/Program.cs
+++ Z4/Program.cs
@@ -1,6 +1,7 @@
 ﻿// Задача 4*(не обязательная): Задайте двумерный массив из целых чисел. Напишите программу, которая удалит
 // строку и столбец, на пересечении которых расположен наименьший элемент массива. Под удалением
 // понимается создание нового двумерного массива без строки и столбца
+
 int ReadInt(string text)
 {
     System.Console.Write(text);
@@ -34,16 +35,57 @@ void PrintMatrix(int[,] matrixForPrint)
         System.Console.WriteLine();
     }
 }
-///--------
 
+int[] FinMinNumberIndexes(int[,] matrix)
+{
+    int[] minNumberPosition = new int[2];
 
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (matrix[i, j] < matrix[minNumberPosition[0], minNumberPosition[1]])
+            {
+                minNumberPosition[0] = i;
+                minNumberPosition[1] = j;
+            }
+        }
+    }
 
-///----------
+    return minNumberPosition;
+}
+
+int[,] DeleteRowAndCol(int[,] oldMatrix, int[] indexes)
+{
+    var newMatrix = new int[oldMatrix.GetLength(0) - 1, oldMatrix.GetLength(1) - 1];
+
+    for (int i = 0, x = 0; i < newMatrix.GetLength(0); i++)
+    {
+        x = i < indexes[0] ? i : i + 1;
+        for (int j = 0, y = 0; j < newMatrix.GetLength(1); j++)
+        {
+            y = j < indexes[1] ? j : j + 1;
+            newMatrix[i, j] = oldMatrix[x, y];
+        }
+    }
+
+    return newMatrix;
+}
+
 int rows = ReadInt("Введите количество строк в матрице: ");
 
 int cols = ReadInt("Введите количество столбцов в матрице: ");
 
-int[,] matrix = GenerateMatrix(rows, cols, 1, 10);
+var matrix = GenerateMatrix(rows, cols, 1, 10);
 
 PrintMatrix(matrix);
+
+System.Console.WriteLine();
+
+var minNumberPosition = FinMinNumberIndexes(matrix);
+
+System.Console.WriteLine(string.Join(" ", minNumberPosition));
+System.Console.WriteLine();
+PrintMatrix(DeleteRowAndCol(matrix, minNumberPosition));
+
 
